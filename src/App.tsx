0918fb0bbc8e658f216ab0039e4a6523ec775e9b
Marko Kvakić <markokvakic@gmail.com>
2020-11-02 22:00:50 +0100

    addition : image state clicking

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,18 @@ import { Image } from 'components/ImageCard/Image';
 
 interface ApiResponse {
   data: Array<Image>;
+  errors: any;
   messages: any;
   status: string;
+}
+
+interface State {
+  view?: boolean;
+  remove?: boolean;
+}
+
+interface ImageState extends State {
+  image: Image;
 }
 
 /**
@@ -16,34 +26,73 @@ interface ApiResponse {
  */
 function App() {
 
-  /** images state */
-  const [ images, setImages ] = useState<Array<Image>>([]);
+  /* images state */
+  const [ imageStates, setImageState ] = useState<Array<ImageState>>([]);
 
-  /** initial images load */
+  /* fetch images on mount */
   useEffect(() => {
     fetch("https://api-dev.insidetrak.interactive1.com/homepage/get-latest-images")
       .then( res => res.json() )
       .then( ( apiResponse: ApiResponse ) => {
-        setImages( apiResponse.data );
+
+        /* set images viewed & remove */
+        const imageStates = apiResponse.data.map( image => {
+          return {
+            image,
+            remove: false,
+            view: false,
+          }
+        })
+
+        /* set image states */
+        setImageState(imageStates);
       });
   }, []);
 
-  /** image card array */
-  const imageCards = images.map( (image, ind) => {
-    return (
-      <ImageCard {...{
-        key: ind,
-        image
-      }}/>
-    )
-  })
+  /* change image state function */
+  const changeImageState = (id: string, state: State) => {
+    const newImageStates = imageStates.map( imageState => {
+      /* check by id and change states */
+      if (imageState.image.id === id) return { ...imageState, ...state }
+      /* or return old state */
+      else return imageState
+    });
+    /* and set new states */
+    setImageState( newImageStates );
+  };
 
-  /** render */
+  /* handle view click */
+  const handleViewClick = (id: string) => {
+    const imageState = imageStates.find( imageState => imageState.image.id === id );
+    const view = imageState ? !imageState.view : false;
+    changeImageState(id, { view });
+  };
+
+  /* handle remove click */
+  const handleRemoveClick = (id: string) => {
+    changeImageState(id, { remove: true });
+  };
+
+  /* image card array, filtered by remove state */
+  const imageCards = imageStates
+    .filter( imageState => !imageState.remove )
+    .map( (imageState, ind) => {
+      const { image, view } = imageState;
+      return (
+        <ImageCard {...{
+          key: ind,
+          image,
+          view,
+          handleViewClick,
+          handleRemoveClick,
+        }}/>
+      )
+    })
+
+  /* render */
   return (
-    <div className="container p-3">
-      <div className="row row-cols-4">
-        { imageCards }
-      </div>
+    <div className="card-columns p-3" style={{columnCount: 4}}>
+      { imageCards }
     </div>
   );
 }
--- src/components/ImageCard/index.tsx
+++ src/components/ImageCard/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Image } from 'components/ImageCard/Image'
+import { Image } from 'components/ImageCard/Image';
+import { FiEye, FiX } from 'react-icons/fi';
+
+type HandleClick = (id: string) => void;
 
 interface Props {
   image: Image;
+  view: boolean | undefined;
+  handleViewClick: HandleClick;
+  handleRemoveClick: HandleClick;
 }
 
 /**
@@ -10,32 +16,56 @@ interface Props {
  */
 function ImageCard(props: Props) {
 
-  // extract image data
+  /* extract props data */
   const {
+    image,
+    view,
+    handleViewClick,
+    handleRemoveClick,
+  } = props;
+
+  /* extract image data */
+  const {
+    id,
     title,
     description,
     publishedOn,
     thumbnailUrls
-  } = props.image;
+  } = image;
 
-  // parse published date
+  /* parse published date */
   const publishedDate = new Date(publishedOn);
 
-  // render
+  /* viewed button className */
+  const buttonClassName = !view ? "btn btn-outline-dark" : "btn btn-outline-primary";
+
+  /* card border className */
+  const cardClassName = !view ? "card" : "card border-primary";
+
+  /* render */
   return (
     <div className="col mb-3">
-      <div className="card h-100">
+      <div className={ cardClassName }>
         <img
           src={ thumbnailUrls.imedia_300 }
-          className="card-img-top"
-          alt="..."/>
-        <b> { title } </b>
-        <p> { description } </p>
-        <p className="card-text">
-          <small className="text-muted">
-            Datum objave: {publishedDate.toLocaleDateString()}
-          </small>
-        </p>
+          className="card-img-top"/>
+        <div className="card-body">
+          <h5 className="card-title"> { title } </h5>
+          <p className="card-text"> { description } </p>
+          <p className="d-flex justify-content-between">
+            <button
+              className={ buttonClassName }
+              onClick={ () => handleViewClick(id) }> <FiEye /> </button>
+            <button
+              className="btn btn-outline-danger"
+              onClick={ () => handleRemoveClick(id)}> <FiX /> </button>
+          </p>
+          <p className="card-text">
+            <small className="text-muted">
+              Datum objave: {publishedDate.toLocaleDateString()}
+            </small>
+          </p>
+        </div>
       </div>
     </div>
   );
